a9ea5e561916ec4f8a595e350e613c3969087205
Chris Argyros <cargyros430@gmail.com>
2022-08-03 22:48:39 -0500

    query variables added

---
+++ javascript.js
@@ -0,0 +1,5 @@
+const optionBtn = document.querySelectorAll('div.optionBtn button');
+const roundResults = document.querySelector('#roundResults');
+const playerPoints = document.querySelector('#playerScore');
+const computerPoints = document.querySelector('#computerScore');
+const resetBtn = document.querySelector('#reset');
